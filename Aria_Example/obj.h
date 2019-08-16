975699133393d131884e3e7aa51217823d687e3d
AriaHiro64 <45049787+AriaHiro64@users.noreply.github.com>
2019-08-16 02:40:48 -0400

    culling fixes

---
+++ Aria_Example/obj.h
@@ -0,0 +1,81 @@
+
+/* Assembly Textures * * * */
+     #define   TEX_EYES_0     0x06000000
+     #define   TEX_EYES_1     0x06000800
+     #define   TEX_EYES_2     0x06001000
+     #define   TEX_EYES_3     0x06001800
+     #define   TEX_EYES_4     0x06002000
+     #define   TEX_EYES_5     0x06002800
+     #define   TEX_EYES_6     0x06003000
+     #define   TEX_EYES_7     0x06003800
+     #define   TEX_MOUTH_0     0x06004000
+     #define   TEX_MOUTH_1     0x06004400
+     #define   TEX_MOUTH_2     0x06004800
+     #define   TEX_MOUTH_3     0x06004C00
+
+/* Display Lists * * * */
+     #define   DL_FOOT_3_R_ZZLMTX     0x060112B0
+     #define   DL_FOOT_2_R_ZZLMTX     0x06011800
+     #define   DL_OCARINA_HAND_R     0x06011BB0
+     #define   DL_FOOT_2_L_ZZLMTX     0x060120B0
+     #define   DL_FOOT_3_L_ZZLMTX     0x060126A0
+     #define   DL_BOOMERANG     0x060139C0
+     #define   DL_FPS_FOREARM_L     0x06013D60
+     #define   DL_FPS_HAND_L     0x06013E20
+     #define   DL_BOW_STRING     0x06014078
+     #define   DL_FPS_HAND_R     0x06014930
+     #define   DL_HILT_2     0x06015CD8
+     #define   DL_BROKEN_BLADE_3     0x06016390
+     #define   DL_HILT_3     0x06016AC0
+     #define   DL_FPS_FOREARM_R     0x06016EF0
+     #define   DL_FPS_HAND_SHOT     0x06017A50
+     #define   DL_CROSSHAIR     0x06017DE8
+     #define   DL_HILT_1     0x06018418
+     #define   DL_SHEATH_1     0x060187F0
+     #define   DL_SHIELD_2     0x06018E88
+     #define   DL_SHIELD_1     0x06019508
+     #define   DL_SHEATH     0x06019D68
+     #define   DL_OCARINA_2     0x0601A088
+     #define   DL_OCARINA_1     0x0601A308
+     #define   DL_HAMMER     0x0601AB18
+     #define   DL_GAUNTLET_HAND_R_ZZLMTX     0x0601AFC8
+     #define   DL_GAUNTLET_HAND_L_ZZLMTX     0x0601B3A8
+     #define   DL_GAUNTLET_FOREARM_R_ZZLMTX     0x0601B768
+     #define   DL_GAUNTLET_FOREARM_L_ZZLMTX     0x0601BB48
+     #define   DL_GAUNTLET_FIST_R_ZZLMTX     0x0601BF48
+     #define   DL_GAUNTLET_FIST_L_ZZLMTX     0x0601C328
+     #define   DL_FIST_R     0x0601C678
+     #define   DL_FIST_L     0x0601CAB8
+     #define   DL_BOW     0x0601DB88
+     #define   DL_BOTTLE_HAND_L     0x0601E288
+     #define   DL_BOTTLE     0x0601E600
+     #define   DL_BLADE_3     0x0601EEE8
+     #define   DL_BLADE_2     0x0601FA70
+     #define   DL_SHIELD_3     0x060200D8
+
+/* Collisions * * * */
+
+/* Hierarchical Display Lists * * * */
+     #define   LIMB_LIMB_1     0x06020E50
+     #define   LIMB_LIMB_3     0x06021888
+     #define   LIMB_LIMB_4     0x06021D80
+     #define   LIMB_LIMB_5     0x06022288
+     #define   LIMB_LIMB_6     0x06022888
+     #define   LIMB_LIMB_7     0x06022C28
+     #define   LIMB_LIMB_8     0x06023020
+     #define   LIMB_LIMB_10     0x06024208
+     #define   LIMB_LIMB_11     0x06025010
+     #define   LIMB_LIMB_12     0x06025ED8
+     #define   LIMB_LIMB_13     0x06026298
+     #define   LIMB_LIMB_14     0x060266F0
+     #define   LIMB_LIMB_15     0x06026B90
+     #define   LIMB_LIMB_16     0x06026E50
+     #define   LIMB_LIMB_17     0x06027248
+     #define   LIMB_LIMB_18     0x06027728
+     #define   LIMB_LIMB_19     0x06027908
+     #define   LIMB_LIMB_20     0x06028558
+
+/* Hierarchies (Skeletons) * * * */
+     #define   SKL_DEFAULT     0x06028A90
+
+/* Base Offset: 0x06000000 */
